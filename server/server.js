bf2cf2b8105a2305e35ddea548e1e38c57a5521e
Daniel De Guzman <50926547+DenDanDin@users.noreply.github.com>
2023-03-04 21:10:19 -0800

    Frontend player.

--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
+const lyricsFinder = require('lyrics-finder');
 const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.post('/refresh', (req, res) => {
     const refreshToken = req.body.refreshToken
     const spotifyApi = new SpotifyWebApi({
-        redirectUri: 'http://localhost:3000',
-        clientId: '8af824c2c8084caa84fb55cdd88148e7',
-        clientSecret: '132459e1a351454ba7813c48df04c951',
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
         refreshToken
     })
 
@@ -35,9 +37,9 @@ app.post('/refresh', (req, res) => {
 app.post('/login', (req, res) => {
     const code = req.body.code;
     const spotifyApi = new SpotifyWebApi({
-        redirectUri: 'http://localhost:3000',
-        clientId: '8af824c2c8084caa84fb55cdd88148e7',
-        clientSecret: '132459e1a351454ba7813c48df04c951'
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
     })
 
     spotifyApi
@@ -53,6 +55,11 @@ app.post('/login', (req, res) => {
             console.log(err)
             res.sendStatus(400)
         })
+})
+
+app.get('/lyrics', async (req, res) => {
+    const lyrics = await lyricsFinder(req.query.artist, req.query.track || "No Lyrics Found")
+    res.json({lyrics})
 })
 
 app.listen(3001)
